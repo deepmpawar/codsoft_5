9677d7b53eff7044f767d413e5cb36e084ed1099
deepmpawar <152501838+deepmpawar@users.noreply.github.com>
2024-07-18 18:59:49 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -1,280 +1,279 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(RecipeApp());
+  runApp(MyApp());
 }
 
-class RecipeApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Recipe App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: SplashScreen(),
+    );
+  }
+}
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Simulate loading data with a delay
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => RecipeListScreen()),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.orange, // Choose your desired background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add your splash screen content here
+            Text(
+              'Recipe App',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'FontMain'
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(), // Or any other loading indicator
+          ],
+        ),
+      ),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class RecipeListScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _RecipeListScreenState createState() => _RecipeListScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  late Future<List<Recipe>> _recipes;
-  String _searchQuery = '';
+class _RecipeListScreenState extends State<RecipeListScreen> {
+  List<Recipe> recipes = [];
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _recipes = ApiService.fetchRecipes();
+    fetchRecipes(''); // Default search query
   }
 
-  void _searchRecipes(String query) {
-    setState(() {
-      _searchQuery = query;
-      _recipes = ApiService.searchRecipes(query);
-    });
+  Future<void> fetchRecipes(String query) async {
+    final String apiKey = '1'; // Replace with your API key
+    final String url = 'https://www.themealdb.com/api/json/v1/$apiKey/search.php?s=$query';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        final List<dynamic> meals = data['meals'];
+
+        setState(() {
+          recipes = meals.map((meal) => Recipe.fromJson(meal)).toList();
+        });
+      } else {
+        throw Exception('Failed to load recipes');
+      }
+    } catch (e) {
+      print('Error: $e');
+      // Handle error, show message to the user
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Recipe App')),
+        title: Center(
+          child: Text(
+            'What do you like to cook today?',
+            style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700, fontFamily: 'FontMain'),
+          ),
+        ),
+        backgroundColor: Colors.orange,
         actions: [
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
-              showSearch(
-                context: context,
-                delegate: RecipeSearchDelegate(
-                  onSearch: _searchRecipes,
-                ),
-              );
+              fetchRecipes(searchController.text);
             },
           ),
         ],
       ),
-      body: FutureBuilder<List<Recipe>>(
-        future: _recipes,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No recipes found'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final recipe = snapshot.data![index];
-                return ListTile(
-                  leading: Image.network(
-                    recipe.imageUrl,
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
-                  title: Text(recipe.title),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RecipeDetailsScreen(recipeId: recipe.id),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/backimg.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter a recipe name',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () {
+                            searchController.clear();
+                          },
+                        ),
                       ),
-                    );
-                  },
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
-  }
-}
-
-class RecipeSearchDelegate extends SearchDelegate {
-  final Function(String) onSearch;
-
-  RecipeSearchDelegate({required this.onSearch});
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    onSearch(query);
-    return Container();
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Container();
-  }
-}
-
-class RecipeDetailsScreen extends StatelessWidget {
-  final String recipeId;
-
-  RecipeDetailsScreen({required this.recipeId});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Recipe Details'),
-      ),
-      body: FutureBuilder<Recipe>(
-        future: ApiService.fetchRecipeDetails(recipeId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('Recipe not found'));
-          } else {
-            final recipe = snapshot.data!;
-            return SingleChildScrollView(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(
-                    recipe.imageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: 250,
-                    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                      if (loadingProgress == null) {
-                        return child;
-                      } else {
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                                : null,
+                      onSubmitted: (value) {
+                        fetchRecipes(value);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Today's Special Dishes...",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, fontFamily: 'FontMain', color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: recipes.isEmpty
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                itemCount: recipes.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 65,
+                    child: ListTile(
+                      leading: ClipOval(
+                          child: Image.network(
+                            recipes[index].strMealThumb,
+                            fit: BoxFit.fill,
+                            height: 65,
+                            width: 65,
+                          )),
+                      title: Text(
+                        recipes[index].strMeal,
+                        style: TextStyle(fontWeight: FontWeight.w700, fontSize: 19),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RecipeDetailScreen(recipe: recipes[index]),
                           ),
                         );
-                      }
-                    },
-                    errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                      return Center(
-                        child: Icon(
-                          Icons.error,
-                          color: Colors.red,
-                          size: 50,
-                        ),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(recipe.title, style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 16.0),
-                  Text('Ingredients', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-                  ...recipe.ingredients.map((ingredient) => Text(ingredient)).toList(),
-                  SizedBox(height: 16.0),
-                  Text('Instructions', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-                  Text(recipe.instructions),
-                ],
+                      },
+                    ),
+                  );
+                },
               ),
-            );
-          }
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class Recipe {
-  final String id;
-  final String title;
-  final String imageUrl;
+  final String idMeal;
+  final String strMeal;
+  final String strMealThumb;
+  final String strInstructions;
   final List<String> ingredients;
-  final String instructions;
+  final List<String> measures;
 
   Recipe({
-    required this.id,
-    required this.title,
-    required this.imageUrl,
+    required this.idMeal,
+    required this.strMeal,
+    required this.strMealThumb,
+    required this.strInstructions,
     required this.ingredients,
-    required this.instructions,
+    required this.measures,
   });
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
+    List<String> ingredients = [];
+    List<String> measures = [];
+
+    for (int i = 1; i <= 20; i++) {
+      if (json['strIngredient$i'] != null && json['strIngredient$i'].isNotEmpty) {
+        ingredients.add(json['strIngredient$i']);
+        measures.add(json['strMeasure$i']);
+      }
+    }
+
     return Recipe(
-      id: json['idMeal'],
-      title: json['strMeal'],
-      imageUrl: json['strMealThumb'],
-      ingredients: List<String>.generate(20, (index) {
-        final ingredient = json['strIngredient${index + 1}'];
-        return ingredient != null && ingredient.isNotEmpty ? ingredient : '';
-      }).where((ingredient) => ingredient.isNotEmpty).toList(),
-      instructions: json['strInstructions'],
+      idMeal: json['idMeal'],
+      strMeal: json['strMeal'],
+      strMealThumb: json['strMealThumb'],
+      strInstructions: json['strInstructions'],
+      ingredients: ingredients,
+      measures: measures,
     );
   }
 }
 
-class ApiService {
-  static const String apiUrl = 'http://10.0.2.2:3000';
+class RecipeDetailScreen extends StatelessWidget {
+  final Recipe recipe;
 
-  static Future<List<Recipe>> fetchRecipes() async {
-    final response = await http.get(Uri.parse('$apiUrl/recipes'));
+  RecipeDetailScreen({required this.recipe});
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((json) => Recipe.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load recipes');
-    }
-  }
-
-  static Future<Recipe> fetchRecipeDetails(String id) async {
-    final response = await http.get(Uri.parse('$apiUrl/recipes/$id'));
-
-    if (response.statusCode == 200) {
-      return Recipe.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load recipe details');
-    }
-  }
-
-  static Future<List<Recipe>> searchRecipes(String query) async {
-    final response = await http.get(Uri.parse('$apiUrl/search?q=$query'));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((json) => Recipe.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to search recipes');
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(recipe.strMeal, style: TextStyle(fontFamily: 'FontMain', fontSize: 34,fontWeight: FontWeight.w700),),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: ClipOval(
+                child: Image.network(
+                  recipe.strMealThumb,
+                  fit: BoxFit.fill,
+                  height: 400,
+                  width: 500,
+                ))),
+            SizedBox(height: 16.0),
+            Center(child: Text(recipe.strMeal, style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold, fontFamily: 'FontMain',color: Colors.red))),
+            SizedBox(height: 20.0),
+            Text('Ingredients:', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+            for (int i = 0; i < recipe.ingredients.length; i++)
+              Text('${recipe.measures[i]} ${recipe.ingredients[i]}', style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 20.0),
+            Text('Instructions:', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+            Text(recipe.strInstructions, style: TextStyle(fontSize: 16.0)),
+          ],
+        ),
+      ),
+    );
   }
 }
+
